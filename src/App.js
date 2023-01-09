f50c71ad8b96c6e9817f5ebc3419701ee74d7c49
Morning <morning@mstars-lab3-05s-iMac.local>
2023-01-09 12:26:34 +0800

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [doo, setDoo] = useState("");
+  const [task, setTask] = useState([])
+  const [count, setCount] = useState(0)
+  function save() {
+    const taskObj={
+      id: task.length,
+      title: doo,
+      isChecked: false
+    }
+    const newArr = [...task]
+    newArr.push(taskObj)
+    setTask(newArr)
+    setDoo("")
+  }
+  function onDone(id){
+    const objList = task.map((val)=>{
+
+      if (val.id ===id){
+        val.isChecked = !val.isChecked
+      }
+      return val;
+    })
+    setTask(objList)
+    countDone()
+  }
+  function countDone() {
+    const arr = task.filter((e)=> e.isChecked === true)
+    setCount(arr.length)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-xxl m-auto">
+      <div className="row ">
+        <h1>To-Do-List</h1>
+        <h2>Total {task.length}. Done {count}</h2>
+        <div className="d-flex">
+          <input
+            id="task"
+            value={doo}
+            type="text"
+            placeholder="Add to-do-list"
+            onChange={(e) => setDoo(e.target.value)}
+          />
+          <button className="btn btn-warning" onClick={save}>
+            Add
+          </button>
+        </div>
+        <div>
+
+            {task.map((e) => (
+              <div className="d-flex">
+                <input type="checkbox" name="" value="" checked={e.isChecked} onChange={() => {onDone(e.id)}}/>
+                <h4>{e.title}</h4>
+                <button className="btn btn-secondary">Edit</button>                
+                <button className="btn btn-danger">Delete</button>                
+              </div>
+            ))}
+        </div>
+      </div>
     </div>
   );
 }
