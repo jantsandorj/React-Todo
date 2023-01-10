c4faaf59f73a2f9e5e8af0414005ab312002b8c3
Morning <morning@mstars-lab3-05s-iMac.local>
2023-01-10 12:30:59 +0800

    delete and edit

--- src/App.js
+++ src/App.js
@@ -2,46 +2,80 @@ import { useState } from "react";
 
 function App() {
   const [doo, setDoo] = useState("");
-  const [task, setTask] = useState([])
-  const [count, setCount] = useState(0)
+  const [task, setTask] = useState([]);
+  const [count, setCount] = useState(0);
   function save() {
-    const taskObj={
+    
+      
+    
+    const taskObj = {
       id: task.length,
       title: doo,
-      isChecked: false
-    }
-    const newArr = [...task]
-    newArr.push(taskObj)
-    setTask(newArr)
-    setDoo("")
+      isChecked: false,
+    };
+   
+        const newArr = [...task];
+        newArr.push(taskObj);
+        setTask(newArr);
+        setDoo("");
+    
   }
-  function onDone(id){
-    const objList = task.map((val)=>{
-
-      if (val.id ===id){
-        val.isChecked = !val.isChecked
+  function delTask(id) {
+    task.map((val) => {
+      if (val.id === id) {
+        const delList = [...task]
+        delList.splice(delList.indexOf(val), 1);
+        setTask(delList)
       }
       return val;
-    })
-    setTask(objList)
-    countDone()
+    });
+  console.log(task);
+  countDone()
+  }
+  function editTask(id) {
+    task.map((val) => {
+      if (val.id === id) {
+        setDoo(val.title)
+        const editList = [...task]
+        editList[id].title = doo
+        setTask(editList);
+      }else if (val.id === undefined){
+        save()
+      }else 
+      return val
+    });
+
+  }
+
+  function onDone(id) {
+    const objList = task.map((val) => {
+      if (val.id === id) {
+        val.isChecked = !val.isChecked;
+      }
+      return val;
+    });
+    setTask(objList);
+    countDone();
   }
   function countDone() {
-    const arr = task.filter((e)=> e.isChecked === true)
-    setCount(arr.length)
+    console.log(task);
+    const arr = task.filter((e) => e.isChecked === true);
+    setCount(arr.length);
   }
 
   return (
     <div className="container-xxl m-auto">
       <div className="row ">
         <h1>To-Do-List</h1>
-        <h2>Total {task.length}. Done {count}</h2>
+        <h2>
+          Total {task.length}. Done {count}
+        </h2>
         <div className="d-flex">
           <input
             id="task"
             value={doo}
             type="text"
-            placeholder="Add to-do-list"
+            placeholder="Add to-do-task"
             onChange={(e) => setDoo(e.target.value)}
           />
           <button className="btn btn-warning" onClick={save}>
@@ -49,15 +83,31 @@ function App() {
           </button>
         </div>
         <div>
-
-            {task.map((e) => (
-              <div className="d-flex">
-                <input type="checkbox" name="" value="" checked={e.isChecked} onChange={() => {onDone(e.id)}}/>
-                <h4>{e.title}</h4>
-                <button className="btn btn-secondary">Edit</button>                
-                <button className="btn btn-danger">Delete</button>                
-              </div>
-            ))}
+          {task.map((e) => (
+            <div className="d-flex">
+              <input
+                type="checkbox"
+                name=""
+                value=""
+                checked={e.isChecked}
+                onChange={() => {
+                  onDone(e.id);
+                }}
+              />
+              <h4>{e.title}</h4>
+              <button className="btn btn-secondary" onClick={() => {
+                  editTask(e.id)
+                }}>Edit</button>
+              <button
+                className="btn btn-danger"
+                onClick={() => {
+                  delTask(e.id);
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          ))}
         </div>
       </div>
     </div>
